932e12d368c9cc58f22c1da6915b7a978958f3fd
snottymoose <rakovanite@gmail.com>
2026-07-13 16:17:42 +0300

    -

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from aiogram.types import LinkPreviewOptions
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
+ADMIN_CHAT_ID = -1004313216807
 
 bot = Bot(
     token=BOT_TOKEN,
